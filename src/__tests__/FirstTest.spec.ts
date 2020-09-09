aedc3bd36af14ececaeda47f87da63924ab60e77
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-09-09 02:08:57 -0300

    Configuração Babel

---
+++ src/__tests__/FirstTest.spec.ts
@@ -0,0 +1,11 @@
+import { User } from "src/entities/User";
+
+test("OK TEST", () => {
+  const user = new User({
+    name: "Thiago Turim",
+    email: "turimthiago@gmail.com",
+    password: "123mudar"
+  });
+
+  expect(user.name).toEqual("Thiago Turim");
+});
