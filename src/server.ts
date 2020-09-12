6d0c0c387275d02870a437e3a3e1acd039cde0fc
Thiago Turim <thiagoturim@Thiagos-MacBook-Pro.local>
2020-09-11 23:27:08 -0300

    configurado typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
+import "reflect-metadata";
+import "./database/connection";
 import { app } from "./app";
 
-app.listen(3333, () => {
-  console.log("Server started on port [3333]");
-});
+app.listen(3333, () => console.log("Server started on port [3333]"));
